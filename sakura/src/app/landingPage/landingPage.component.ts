24fd16e6052376bfb93661775ece80c305626933
Alex Gitel <alexeygitel@gmail.com>
2025-06-07 20:36:42 +0200

    started sakura

---
+++ sakura/src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component'
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent],
+    template: `
+    <section>
+        <div class="nav_div"><app-navbar></app-navbar></div>
+        <h1 class="fontRaleway"> SAKURA RAMEN </h1>
+        <h6 class="fontRaleway"> BEST RAMEN IN TOWN</h6>
+        </section>
+    `,
+    styleUrls: ['./landingPage.component.scss']
+})
+
+export class LandingPageComponent { }
